7335e12b711e2da42eec8239eb0ac294b9b65b12
Greg Qiu <greg.qiu@bell.ca>
2022-07-15 13:38:03 -0400

    first commit

---
+++ Ca.Bell.Bbm.RpaTektonProveConcept.Api/Controllers/ProveConceptStatusController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ca.Bell.Bbm.RpaTektonProveConcept.Api.Controllers
+{
+    [Route("api/proveconcept")]
+    [ApiController]
+    public class ProveConceptStatusController : ControllerBase
+    {
+        
+        [HttpGet("status")]
+        public string GetStatus()
+        {
+            return "success";
+        }
+
+
+     
+    }
+}
